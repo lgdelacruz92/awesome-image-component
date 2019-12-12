83506890f82c99e595007db0afa8241e759d3b18
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-12 05:16:16 -0800

    Added a button

---
+++ src/components/mybutton.js
@@ -0,0 +1,7 @@
+import React from "react";
+import * as MUI from "@material-ui/core";
+
+const MyButton = props => {
+  return <MUI.Button color="primary">MY Button</MUI.Button>;
+};
+export default MyButton;
