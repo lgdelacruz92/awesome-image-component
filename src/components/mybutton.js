7133b3427b54948a9ebc064a73e2eeb711663ef4
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-12 05:47:04 -0800

    Setup container

--- src/components/mybutton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import * as MUI from "@material-ui/core";
-
-const MyButton = props => {
-  return <MUI.Button color="primary">MY Button</MUI.Button>;
-};
-export default MyButton;
