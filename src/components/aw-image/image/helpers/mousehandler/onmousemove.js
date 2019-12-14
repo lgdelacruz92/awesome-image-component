9c1dd51a2f7001555e8493e822f850b25af12132
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-14 11:25:12 -0800

    Refactor

--- src/components/aw-image/image/helpers/mousehandler/onmousemove.js
+++ src/components/aw-image/image/helpers/mousehandler/onmousemove.js
@@ -1,19 +1,19 @@
-import { isTargetSelf, isEventTranslate } from "./utils";
+import { isTargetSelf, isEventTranslate, calVec, addVec } from "./utils";
 
 export const onMouseMove = props => {
   const { eventState, id, dispatchImageState, e } = props;
   if (isTargetSelf(eventState, id) && isEventTranslate(eventState)) {
-    const startX = eventState.startX;
-    const startY = eventState.startY;
-    const x = e.clientX - startX;
-    const y = e.clientY - startY;
-    const posX = eventState.posX;
-    const posY = eventState.posY;
+    const dir = calVec(
+      { x: eventState.startX, y: eventState.startY },
+      { x: e.clientX, y: e.clientY }
+    );
+    const start = { x: eventState.posX, y: eventState.posY };
+    const newPos = addVec(start, dir);
 
     dispatchImageState({
       type: "move",
-      x: posX + x,
-      y: posY + y
+      x: newPos.x,
+      y: newPos.y
     });
   }
 };
--- src/components/aw-image/image/helpers/mousehandler/utils.js
+++ src/components/aw-image/image/helpers/mousehandler/utils.js
@@ -5,3 +5,11 @@ export const isEventTranslate = event => {
 export const isTargetSelf = (event, id) => {
   return event.targetId === id;
 };
+
+export const calVec = (startPos, endPos) => {
+  return { x: endPos.x - startPos.x, y: endPos.y - startPos.y };
+};
+
+export const addVec = (v1, v2) => {
+  return { x: v1.x + v2.x, y: v1.y + v2.y };
+};
