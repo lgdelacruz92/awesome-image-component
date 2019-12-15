ad4a7e26d07e74034a8bf328fb0c9c349f9067df
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-15 09:47:56 -0800

    Complete

--- src/components/aw-image/image/imagetransformers/toptransformer/calcNewHeight.spec.js
+++ src/components/aw-image/image/imagetransformers/toptransformer/calcNewHeight.spec.js
@@ -1,23 +1,26 @@
 import { calcNewHeight } from "./calcNewHeight";
 describe("Transformer > TopTransformer > Calculate new height", () => {
   test("Transform up", () => {
+    const MINHEIGHT = 25;
     const eventState = { startY: 100, origH: 100 };
     const mouseEvent = { clientY: 100 };
     for (let i = mouseEvent.clientY; i >= 0; i--) {
-      const result = calcNewHeight({ clientY: i, eventState });
+      const result = calcNewHeight({ clientY: i, eventState, MINHEIGHT });
       expect(result).toBe(100 + (100 - i));
     }
   });
 
   test("Transform down", () => {
+    const MINHEIGHT = 25;
+
     const eventState = { startY: 100, origH: 100 };
     const mouseEvent = { clientY: 100 };
     for (let i = mouseEvent.clientY; i < 210; i++) {
-      const result = calcNewHeight({ clientY: i, eventState });
-      if (i <= 200) {
+      const result = calcNewHeight({ clientY: i, eventState, MINHEIGHT });
+      if (i <= 175) {
         expect(result).toBe(100 - (i - 100));
       } else {
-        expect(result).toBe(0);
+        expect(result).toBe(MINHEIGHT);
       }
     }
   });
--- src/components/aw-image/image/imagetransformers/toptransformer/calcNewY.spec.js
+++ src/components/aw-image/image/imagetransformers/toptransformer/calcNewY.spec.js
@@ -2,23 +2,25 @@ import { calcNewY } from "./calcNewY";
 
 describe("Transformer > TopTransformer > Calculate New Y", () => {
   test("Transform up", () => {
+    const MINHEIGHT = 25;
     const eventState = { startY: 100, origY: 100, origH: 100 };
     const mouseEvent = { clientY: 100 };
     for (let i = mouseEvent.clientY; i >= 0; i--) {
-      const newY = calcNewY({ eventState, clientY: i });
+      const newY = calcNewY({ eventState, clientY: i, MINHEIGHT });
       expect(newY).toBe(i);
     }
   });
 
   test("Transform down", () => {
+    const MINHEIGHT = 25;
     const eventState = { startY: 100, origY: 100, origH: 100 };
     const mouseEvent = { clientY: 100 };
     for (let i = mouseEvent.clientY; i < 210; i++) {
-      const newY = calcNewY({ eventState, clientY: i });
-      if (i <= 200) {
+      const newY = calcNewY({ eventState, clientY: i, MINHEIGHT });
+      if (i <= 200 - MINHEIGHT) {
         expect(newY).toBe(i);
       } else {
-        expect(newY).toBe(200);
+        expect(newY).toBe(200 - MINHEIGHT);
       }
     }
   });
