c7a9f988382acb8e21713439ecbd49794a4a37cd
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-15 10:45:30 -0800

    Calc new width complete

--- src/components/aw-image/image/imagetransformers/lefttransformer/calcNewWidth.spec.js
+++ src/components/aw-image/image/imagetransformers/lefttransformer/calcNewWidth.spec.js
@@ -9,4 +9,18 @@ describe("Transformer > Left Transformer > Calculate new width", () => {
       expect(newW).toBe(100 + (100 - i));
     }
   });
+
+  test("To the right", () => {
+    const eventState = { startX: 100, origW: 100 };
+    const MINWIDTH = 25;
+    const mouseEvent = { clientX: 100 };
+    for (let i = mouseEvent.clientX; i < 210; i++) {
+      const newW = calcNewWidth({ eventState, clientX: i, MINWIDTH });
+      if (i <= 200 - MINWIDTH) {
+        expect(newW).toBe(100 - (i - 100));
+      } else {
+        expect(newW).toBe(MINWIDTH);
+      }
+    }
+  });
 });
